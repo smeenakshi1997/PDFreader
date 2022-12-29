4b0e8b87a2e8e3fb6e62da6c85390c90590b9fd2
smeenakshi1997 <smeenakshi1997@gmail.com>
2022-12-29 20:45:48 +0530

    Update pdf_reader_GEM.py

--- pdf_reader_GEM.py
+++ pdf_reader_GEM.py
@@ -35,28 +35,28 @@ for i in range(len(pages)):
        text = pytesseract.image_to_string(cropped)
        text = "".join(text).strip('\n')
        all_text.append(text)
-       for a in all_text:
-          #All the items are displayed as lists. Use normal python string manipulators to break it in chunks to get relevant text.
-          bid_re=re.compile(r"Bid/RA/PBP No.:((?:.*\n){1,3})")
-          bid = [x.strip() for x in bid_re.findall(a)]
-          print(bid)
-          company_re=re.compile(r"Company Name:((?:.*\n){1,3})")
-          comp_name = [x.strip() for x in company_re.findall(a)]
-          print(comp_name)
-          contact_re=re.compile(r"Contact No.:((?:.*\n){1,3})")
-          contact = [x.strip() for x in contact_re.findall(a)]
-          print(contact) #Contact list has two items. 1st one : consignee data, 2nd one : Service provider data
-          contract_re=re.compile(r"Contract No:((?:.*\n){1,3})")
-          contract = [x.strip() for x in contract_re.findall(a)]
-          print(contract)
-          generated_re=re.compile(r"Generated Date:((?:.*\n){1,3})")
-          gen_date = [x.strip() for x in generated_re.findall(a)]
-          print(gen_date)
-          start_re=re.compile(r"Start Date((?:.*\n){1,3})")
-          start_date = [x.strip() for x in start_re.findall(a)]
-          print(start_date)
-          end_re=re.compile(r"End Date((?:.*\n){1,3})")
-          end_date = [x.strip() for x in end_re.findall(a)]
-          print(end_date)
-          break
-   
+      
+for a in all_text:
+    #All the items are displayed as lists. Use normal python string manipulators to break it in chunks to get relevant text.
+    bid_re=re.compile(r"Bid/RA/PBP No.:((?:.*\n){1,3})")
+    bid = [x.strip() for x in bid_re.findall(a)]
+    print(bid)
+    company_re=re.compile(r"Company Name:((?:.*\n){1,3})")
+    comp_name = [x.strip() for x in company_re.findall(a)]
+    print(comp_name)
+    contact_re=re.compile(r"Contact No.:((?:.*\n){1,3})")
+    contact = [x.strip() for x in contact_re.findall(a)]
+    print(contact) #Contact list has two items. 1st one : consignee data, 2nd one : Service provider data
+    contract_re=re.compile(r"Contract No:((?:.*\n){1,3})")
+    contract = [x.strip() for x in contract_re.findall(a)]
+    print(contract)
+    generated_re=re.compile(r"Generated Date:((?:.*\n){1,3})")
+    gen_date = [x.strip() for x in generated_re.findall(a)]
+    print(gen_date)
+    start_re=re.compile(r"Start Date((?:.*\n){1,3})")
+    start_date = [x.strip() for x in start_re.findall(a)]
+    print(start_date)
+    end_re=re.compile(r"End Date((?:.*\n){1,3})")
+    end_date = [x.strip() for x in end_re.findall(a)]
+    print(end_date)
+    break      
